2692ae6acf52da0b7e724ab808b2dca5a9051769
Alex Valle <alex.valle@gmail.com>
2023-01-13 15:59:36 -0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/main.css"
+  }
+})
