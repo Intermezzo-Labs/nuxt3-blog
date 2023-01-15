b4af9162c94f42c41ba3a993e8ca5aa776d35160
Alex <alex.valle@gmail.com>
2023-01-15 10:00:33 -0500

    fix: DRY, parent class will apply to children

--- components/core/Navigation.vue
+++ components/core/Navigation.vue
@@ -1,8 +1,7 @@
 <template>
-  <nav class="space-x-8">
-    <NuxtLink to="/contact" class="text-lg font-medium mr-4">Contact Us</NuxtLink>
-    <NuxtLink to="/about" class="text-lg font-medium mr-4">About</NuxtLink>
-    <NuxtLink to="/blog" class="text-lg font-medium mr-4">Blog</NuxtLink>
-    <NuxtLink to="/services" class="text-lg font-medium mr-4">Services</NuxtLink>
+  <nav class="space-x-8 text-lg font-medium">
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/blog">Blog</NuxtLink>
+    <NuxtLink to="/services">Services</NuxtLink>
   </nav>
 </template>
