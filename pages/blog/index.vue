54224fbb35683ecadbb31b942a4609497c967a06
Alex <alex.valle@gmail.com>
2023-01-15 12:21:12 -0500

    added links to blog pages

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,13 +3,29 @@
     <main>
       <ContentDoc />
     </main>
-    <aside>
-      <ContentList path="/blog" v-slot="{ list }">
-        <div v-for="article in list" :key="article._path">
-          <h2>{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
-        </div>
-      </ContentList>
-    </aside>
+    <section>
+      <div class="container mx-auto max-w-3xl grid grid-cols-2 gap-16">
+        <ContentList path="/blog" v-slot="{ list }">
+          <article v-for="article in list" :key="article._path">
+            <div>
+              <NuxtLink :to="article._path">
+                <figure>
+                  <picture>
+                    <img src="https://picsum.photos/640/420" alt="" />
+                  </picture>
+                </figure>
+              </NuxtLink>
+            </div>
+            <div class="space-y-4 pt-4">
+              <p>27 octobre 2022 All Harvey Solidarity</p>
+              <h2 class="text-2xl">
+                <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+              </h2>
+              <p>{{ article.description }}</p>
+            </div>
+          </article>
+        </ContentList>
+      </div>
+    </section>
   </NuxtLayout>
 </template>
