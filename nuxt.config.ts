fdcc33bb93933fe65791289ca7c87590e0a076ea
Alex <alex.valle@gmail.com>
2023-01-15 10:30:05 -0500

    enable document driven

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   content: {
     // https://content.nuxtjs.org/api/configuration
+    documentDriven: {},
   },
   tailwindcss: {
     cssPath: "~/assets/css/main.css",
