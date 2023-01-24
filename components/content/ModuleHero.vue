7c9d943bb22633127f620e4649aaea7a8afd999d
myenny <my@gmail.com>
2023-01-23 19:08:57 -0500

    h-screen

--- components/content/ModuleHero.vue
+++ components/content/ModuleHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="py-24 bg-black">
+  <section class="py-24 bg-black h-screen">
     <div class="container mx-auto text-center space-y-8 max-w-lg">
       <slot />
     </div>
