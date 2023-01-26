446e8b4fbab615ecc6390b4d6a415f607b815666
Alex <alex.valle@gmail.com>
2023-01-26 01:05:06 -0500

    fix mobile view

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <CoreHeader />
-    <div class="grid container mx-auto max-w-4xl grid-cols-3 py-12">
-      <main class="prose col-span-2">
+    <div
+      class="grid container mx-auto max-w-4xl sm:grid-cols-3 p-4 sm:py-12 sm:px-0 gap-12"
+    >
+      <main class="prose sm:col-span-2">
         <slot />
       </main>
       <aside>more blog posts</aside>
