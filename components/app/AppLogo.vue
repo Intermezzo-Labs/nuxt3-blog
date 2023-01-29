1248c647059b8c70e77f3ef4ded230f1969f7f4a
Alex <alex.valle@gmail.com>
2023-01-29 00:46:29 -0500

    adjust line height

--- components/app/AppLogo.vue
+++ components/app/AppLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="text-lg sm:text-2xl uppercase font-black tracking-widest leading-none"
+    class="text-lg sm:text-2xl uppercase font-black tracking-widest !leading-6"
     >Intermezzo
     <span class="hidden sm:inline">Labs</span>
   </span>
